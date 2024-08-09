1b0847982bd3e02850ec17e0b4f8a4663c5ae263
sonia <soniakhaan927@gmail.com>
2024-08-09 22:31:37 +0500

    finialize pages

--- components/Topics/sideCard/index.js
+++ components/Topics/sideCard/index.js
@@ -45,7 +45,7 @@ const SideCard = ({post,type}) => {
                   unoptimized
                 />
                 <div>
-                  <h1 className="mb-0">{game.title}</h1>
+                  <h5 className="mb-0">{game.title}</h5>
                   {/*<p className={`mb-0`}>{game.developer}</p>*/}
                   <p>
                     {game.rating} <i className="fas fa-star"></i>
--- src/app/privacy-policy/page.js
+++ src/app/privacy-policy/page.js
@@ -1,8 +1,121 @@
 import { Row, Col, Container } from "react-bootstrap";
 import PrivacyPolicy from "../../../components/tools/privacyPolicy";
 import SideCard from "../../../components/Topics/sideCard";
+import { gql } from '@apollo/client';
+import client from '../../../apollo-client';
+const GET_SIDE_APPS_AND_GAMES = gql`
+  query GetSideAppsAndGames {
+    sideApps: blogPosts(
+      filters: {
+        category: { PageCategory: { eq: "Apps" } }
+        isSideCardShow: { eq: true }
+      }
+    ) {
+      data {
+      id
+      attributes {
+        title
+        rating
+        slug
+        Applogo
+        Image01
+        Image03
+        Image04
+        Image05
+        createdAt
+        subtitle
+      publisher
+        post
+downloads
+category{
+          data{
+            attributes{
+              name
+            }
+          }
+        }
+        size
+        featuredImage {
+          data {
+            attributes {
+              url
+            }
+          }
+        }
 
-const Tools = () => {
+        category {
+          data {
+            id
+            attributes {
+              PageCategory
+            }
+          }
+        }
+      }
+    }
+      }
+    
+    sideGames: blogPosts(
+      filters: {
+        category: { PageCategory: { eq: "Games" } }
+        isSideCardShow: { eq: true }
+      }
+    ) {
+      data {
+      id
+      attributes {
+        title
+        rating
+        slug
+        Applogo
+        Image01
+        Image03
+        Image04
+        Image05
+        subtitle
+      publisher
+      createdAt
+        post
+downloads
+category{
+          data{
+            attributes{
+              name
+            }
+          }
+        }
+        size
+        featuredImage {
+          data {
+            attributes {
+              url
+            }
+          }
+        }
+
+        category {
+          data {
+            id
+            attributes {
+              PageCategory
+            }
+          }
+        }
+      }
+    }
+    }
+  }
+`;
+
+const Tools =async () => {
+
+  const postResponse = await client.query({
+    query: GET_SIDE_APPS_AND_GAMES
+  });
+
+
+  const apps = postResponse?.data?.sideApps?.data;
+  const games = postResponse?.data?.sideGames?.data;
   return (
     <>
       <Container className="TopicsMain">
@@ -11,8 +124,8 @@ const Tools = () => {
             <PrivacyPolicy />
           </Col>
           <Col className={`col-sm-12 col-md-4 col-xxxl-4`}>
-            <SideCard />
-            <SideCard />
+            <SideCard post={games} type='Games' />
+            <SideCard post={apps} type='Apps' />
           </Col>
         </Row>
       </Container>
